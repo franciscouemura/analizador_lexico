22ce0e636d042bcce23a25513dc4291ff46f4448
Murilo <murilom.aikido77@gmail.com>
2019-03-21 18:15:21 -0300

    roteiro de trabalho e divisao das funcoes

---
+++ roteiro.java
@@ -0,0 +1,19 @@
+public void analisadorSintatico(){
+		analisadorLexico()
+		NaoTerminaldePartida()
+		Se Token != Eof então
+			Erro('Fim de arquivo não esperado')
+}
+
+public void NaoTerminais(){
+	criar funcoes recursivas da linguagens
+}   -----37 funcoes
+
+public Lexema identificador(){
+
+	classificar palavras reservadas
+
+}
+
+
+criar tabela de simbolos
